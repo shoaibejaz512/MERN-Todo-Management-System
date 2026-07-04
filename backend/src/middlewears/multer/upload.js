074ed795565e599c3e9bde560a72ca9ff68bade2
shoaibejaz512 <shoaibejaz512@gmail.com>
2026-07-04 22:58:17 +0500

    install some pakage and setup the config file of multer and cloudinary

---
+++ backend/src/middlewears/multer/upload.js
@@ -0,0 +1,23 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/webp", "image/jpg"];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+  fileFilter,
+});
+
+export default upload;
